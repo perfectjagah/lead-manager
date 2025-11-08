7820f7f7cc552bd2b9b4e9bf38cef253fe56a842
perfect jagah <perfectjagah@gmail.com>
2025-11-09 01:12:34 +0530

    Prepare for deployment

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App";
 import { ConfigProvider } from "antd";
-import { HashRouter } from "react-router-dom";
+// import { HashRouter } from "react-router-dom";
 // Custom theme configuration
 const theme = {
   token: {
@@ -18,9 +18,9 @@ const theme = {
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ConfigProvider theme={theme}>
-      <HashRouter>
-        <App />
-      </HashRouter>
+      {/* <HashRouter> */}
+      <App />
+      {/* </HashRouter> */}
     </ConfigProvider>
   </React.StrictMode>
 );
